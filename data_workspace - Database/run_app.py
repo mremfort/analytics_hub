4f1685dc4839a1fa1dd3afa28c86d5d5bd71e08e
mremfort <mremfort@beatriceadvisors.com>
2025-04-10 10:42:31 -0400

    Delete data_workspace - Database directory

--- data_workspace - Database/run_app.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-from Beatrice_Advisors import display_beatrice
-from Christina_Lewis import display_christina
-
-# Full path to the app.py file
-subprocess.call("streamlit run app.py --server.port=8080")
